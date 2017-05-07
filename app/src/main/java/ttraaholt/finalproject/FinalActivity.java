96f5e319eb10b140b8d8e07f814aa23e6fa1e24e
ttraaholt <ttraaholt@css.edu>
2017-05-07 13:34:35 -0500

    have to swap classes

--- app/src/main/java/ttraaholt/finalproject/FinalActivity.java
+++ app/src/main/java/ttraaholt/finalproject/FinalActivity.java
@@ -3,26 +3,73 @@ package ttraaholt.finalproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class FinalActivity extends AppCompatActivity {
 
-    TextView tvResult;
+    TextView tvDay;
+    TextView tvNote1;
+    Button buttonSignOut;
+    Button buttonNewNote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
 
+        showResult();
+        buttonNewNote();
+        buttonSignOut();
+    }
+
+
+    private void showResult() {
+
         Intent Intent = getIntent();
         //Place the Strings passed into three new variables first, last, and spinnerGender.
-        String first = Intent.getStringExtra("firstName");
-        String last = Intent.getStringExtra("lastName");
-        String spinnerGender = Intent.getStringExtra("spinGender");
+        String first = Intent.getStringExtra("firstNote");
+        String spinnerDay = Intent.getStringExtra("spinDay");
+
+        tvDay = (TextView) findViewById(R.id.textViewDay);
+        tvNote1 = (TextView) findViewById(R.id.textViewNote1);
+
+        tvDay.setText("Day: " + spinnerDay);
+        tvNote1.setText("Note: " + first);
+    }
+
+    private void buttonSignOut() {
+        buttonSignOut = (Button) findViewById(R.id.buttonSignOut);
+        buttonSignOut.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+    }
+
+    private void buttonNewNote() {
+        buttonNewNote = (Button) findViewById(R.id.buttonNewNote);
+        buttonNewNote.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 
 
-        tvResult = (TextView) findViewById(R.id.tvResult);
+    //signOut() method that signs out the user.
+    private void signOut() {
+        startActivity(new Intent(FinalActivity.this, SecondActivity.class));
 
-        tvResult.setText(first + last + spinnerGender);
     }
 }
